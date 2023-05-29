5d12fa4b992cb95b1796badeed3d87d0dcf48a2b
Paulo Brocco <70035777+OxxOrcus@users.noreply.github.com>
2023-05-29 23:22:35 +0100

    first commit

---
+++ funcoes_start.py
@@ -0,0 +1,10 @@
+#
+# Arquivo com exemplos de Funcoes
+#
+
+# definindo uma funcao basica
+def func1():
+    print('eu sou uma funcao')
+# funcao que recebe argumentos
+
+# funcao que retorna um valor
